4a46b0b069bd62faecbc3b8d254db00ae4280f3d
DEMNATI Anasse <demnati_anasse@emsi-edu.ma>
2022-06-30 11:37:21 -0700

    Générer les Components, Services et Models

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AddAddressComponent } from './compoonents/add-address/add-address.component';
+import { EditAddressComponent } from './compoonents/edit-address/edit-address.component';
+import { ListAddressComponent } from './compoonents/list-address/list-address.component';
+import { NavbarComponent } from './compoonents/partials/navbar/navbar.component';
+import { PageNotFoundComponent } from './compoonents/partials/page-not-found/page-not-found.component';
+import { LoginComponent } from './compoonents/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddAddressComponent,
+    EditAddressComponent,
+    ListAddressComponent,
+    NavbarComponent,
+    PageNotFoundComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
