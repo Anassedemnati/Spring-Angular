31adf18bf492b35e3c8995966cad04c58a942f6d
DEMNATI Anasse <demnati_anasse@emsi-edu.ma>
2022-07-01 21:32:29 +0100

    Récupérer la liste des adresses

--- src/app/compoonents/list-address/list-address.component.ts
+++ src/app/compoonents/list-address/list-address.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {AddressService} from "../../services/address.service";
 
 @Component({
   selector: 'app-list-address',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListAddressComponent implements OnInit {
 
-  constructor() { }
+  constructor(private addressService:AddressService) { }
 
   ngOnInit(): void {
+    this.getAllAddresses();
+  }
+  getAllAddresses(){
+    this.addressService.getAll().subscribe(res=>{
+      console.log(res);
+    })
   }
 
 }
